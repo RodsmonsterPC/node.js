2d00a15b0e1acc5d776f8200d3f26f53bf1d5c0e
Rods <rodolfo.perez01@iest.edu.mx>
2023-04-18 17:46:46 -0500

    Crud con express y mongoose

--- s9_api_mongoose_express/routers/koders.router.js
+++ s9_api_mongoose_express/routers/koders.router.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { request } from "express";
 import { Koder } from "../models/koders.module.js";
 
 const router = express.Router();
 
-router.get("/", async (reject, response) => {
+router.get("/", async (request, response) => {
   const allKoders = await Koder.find({});
 
   response.json({
@@ -13,6 +13,49 @@ router.get("/", async (reject, response) => {
     },
   });
 });
-router.post("/", (response, reject) => {});
+router.post("/", async (request, response) => {
+  let newKoder = request.body;
+
+  const koderCreated = await Koder.create(newKoder);
+  response.json({
+    sucess: true,
+    data: {
+      message: "Se ah creado el Koder",
+    },
+  });
+  console.log(koderCreated);
+});
+
+router.patch("/:idKoder", async (request, response) => {
+  let infoToUpdated = request.body;
+
+  const koderId = request.params.idKoder;
+
+  const updated = await Koder.findByIdAndUpdate(koderId, infoToUpdated, {
+    new: true,
+  });
+
+  response.json({
+    sucess: true,
+    data: {
+      message: "Se ah actualizado el Koder",
+    },
+  });
+  console.log(updated);
+});
+
+router.delete("/:idKoder", async (request, response) => {
+  const koderId = request.params.idKoder;
+
+  const deleteKoder = await Koder.findByIdAndDelete(koderId);
+
+  response.json({
+    sucess: true,
+    data: {
+      message: "Se ah eliminado el Koder",
+    },
+  });
+  console.log(deleteKoder);
+});
 
 export default router;
